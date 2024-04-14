5585b7f619ab6ac5fe923f983dad3927dfbe120e
sergei_dobri <teplichkinteplichkinov@gmail.com>
2024-04-14 17:19:05 +0300

    Edited unittests, added official Schneier vectors file, edited pi-keys generator in BlowCrypt class

--- test_blowfish.py
+++ test_blowfish.py
@@ -1,46 +1,35 @@
-from os import urandom
-from random import randint
 import blowfish_very_first_py_realization as bl
 import unittest
 
 
 class BlowfishTest(unittest.TestCase):
 
-    def test_ets(self):
-        results = []
-        num = 8
-        for _ in range(1000):
-            cnt = randint(4, 56)
-            key = b''
-            for _ in range(cnt):
-                key += urandom(1)
-            cipher = bl.BlowCrypt(key)
-            block = urandom(num)
-            encr_block = b''.join(cipher.encrypt_ets(block))
-            decr_block = b''.join(cipher.decrypt_ets(encr_block))
-            results.append(decr_block == block)
-            num += 8
-        assert all(results)
+    def setUp(self):
+        self.vectors = self.load_vectors()
 
-    def test_cbc(self):
-        results = []
-        num = 8
-        for _ in range(1000):
-            cnt = randint(4, 56)
-            key = b''
-            for _ in range(cnt):
-                key += urandom(1)
-            iv = urandom(8)
-            cipher = bl.BlowCrypt(key)
-            block = urandom(num)
-            encr_block = b''.join(cipher.encrypt_cbc(block, iv))
-            decr_block = b''.join(cipher.decrypt_cbc(encr_block, iv))
-            results.append(decr_block == block)
-            num += 8
-        assert all(results)
+    def load_vectors(self):
+        vectors = []
+        with open('blowfish_vectors', 'r') as file:
+            for line in file.readlines():
+                key, clear_bytes, cipher_bytes = map(self.hex_str_to_bytes, line.split())
+                vectors.append((key, clear_bytes, cipher_bytes))
+        return vectors
+
+    def test_ets_encrypt_decrypt(self):
+        for key, clear_bytes, cipher_bytes in self.vectors:
+            my_bytes = b''.join(bl.BlowCrypt(key).encrypt_ets(clear_bytes))
+            self.assertEqual(my_bytes, cipher_bytes)
+
+            self.assertEqual(b''.join(bl.BlowCrypt(key).decrypt_ets(my_bytes)), clear_bytes)
+
+
+    @staticmethod
+    def hex_str_to_bytes(data):
+        res = b''
+        for i in range(0, len(data), 2):
+            res += int(data[i:i+2], 16).to_bytes()
+        return res
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
